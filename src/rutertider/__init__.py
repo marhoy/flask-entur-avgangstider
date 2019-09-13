d72ffe7f5f1ba464b085c91bac07a448e3ffea57
Martin Høy <marhoy@gmail.com>
2019-09-13 10:33:56 +0200

    Update

--- src/rutertider/__init__.py
+++ src/rutertider/__init__.py
@@ -1,2 +1,5 @@
 from rutertider.entur_api import get_departures, get_situations
 from rutertider.flask_app import create_app
+
+# Satisfy the PEP8 linter
+__all__ = ["get_departures", "get_situations", "create_app"]
