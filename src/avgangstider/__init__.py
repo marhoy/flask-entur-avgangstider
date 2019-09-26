496f338baaddb28a294d31405fbae5a9930a3715
Martin Høy <marhoy@gmail.com>
2019-09-26 14:02:57 +0200

    Import classes as well

--- src/avgangstider/__init__.py
+++ src/avgangstider/__init__.py
@@ -1,12 +1,14 @@
 import logging
 
+from avgangstider.classes import Departure, Situation
 from avgangstider.entur_api import get_departures, get_situations
 from avgangstider.flask_app import create_app
 
 __version__ = "0.1.0"
 
 # Satisfy the PEP8 linter
-__all__ = ["get_departures", "get_situations", "create_app"]
+__all__ = ["get_departures", "get_situations", "Departure", "Situation",
+           "create_app"]
 
 # Set up package-wide logging configuration
 logging.basicConfig(format='[%(levelname)s] %(name)s(%(lineno)s): %(message)s',
